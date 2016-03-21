2e218554cfe4171a6a1f4173700d7598b4a542d5
Ricardo Izquierdo Martín <r_y_g@hotmail.com>
2016-03-21 17:15:17 +0100

    Crear - Proyecto - CommunityWebMVC

---
+++ CommunityWebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CommunityWebMVC.MvcApplication" Language="C#" %>
---
+++ CommunityWebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommunityWebMVC.Startup))]
+namespace CommunityWebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
